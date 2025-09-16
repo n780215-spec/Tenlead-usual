862b5ccb8b709158e625baa3cf9c72f5a39ca2c4
n780215-spec <n780215@gmail.com>
2025-09-16 18:27:11 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,37 @@
-const CACHE_NAME = "shopping-list-cache-v1";
+const CACHE_NAME = "tenlee-cache-v1";
 const urlsToCache = [
-  "./",
   "./index.html",
   "./manifest.json",
+  "./service-worker.js",
   "./images/icon-192.png",
-  "./images/icon-512.png"
+  "./images/icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
 ];
 
-// 安裝 Service Worker 並快取必要資源
+// 安裝 Service Worker
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log("Caching files...");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// 啟用階段，清理舊快取
+// 啟用 Service Worker
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
+          if (name !== CACHE_NAME) return caches.delete(name);
         })
       );
     })
   );
 });
 
-// 攔截請求，支援離線存取
+// 攔截 fetch 請求
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
